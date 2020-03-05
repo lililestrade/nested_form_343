53d87814cb2bd10f8a3ab5b77d6350dc8c5b56ab
Lucile Lestrade <lucilecharly.lestrade@gmail.com>
2020-03-05 12:18:43 +0100

    add controller with index new and create (include nested form)

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,6 @@
 class Project < ApplicationRecord
-  has_many :tasks
-
   has_many :tasks, inverse_of: :project
+  validates :name, presence: true
   accepts_nested_attributes_for :tasks,
                                 reject_if: proc { |attributes| attributes[:name].blank? },
                                 allow_destroy: true
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ApplicationRecord
   belongs_to :project, inverse_of: :tasks
+  validates :name, presence: true
+  validates :name, length: { minimum: 3 }
 end
