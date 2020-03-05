0bc9146256447794e959b7210fa5f1e71a7a9a64
Lucile Lestrade <lucilecharly.lestrade@gmail.com>
2020-03-05 11:47:50 +0100

    add custom associations between project and tasks

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,9 @@
 class Project < ApplicationRecord
+  has_many :tasks
+
+  has_many :tasks, inverse_of: :project
+  accepts_nested_attributes_for :tasks,
+                                reject_if: proc { |attributes| attributes[:name].blank? },
+                                allow_destroy: true
+
 end
